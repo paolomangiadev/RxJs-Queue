5885887bd4eb1ea5b4f1863fe909656495ae34e5
paolomangiadev <paolo.mangia.dev@gmail.com>
2019-10-12 21:44:19 +0100

    Describe code

--- index.ts
+++ index.ts
@@ -9,15 +9,20 @@ class CallsGetQueue {
       mergeMap((action: any) => action(), 1));
   }
 
+  // Add action to queue
   addToQueue(action) {
     this.queue.next(action);
   }
 }
 
+// Async action
 const hello = (str, timeout) => 
   () => of(str).pipe(delay(timeout));
+
 const instance = new CallsGetQueue();
 instance.results.subscribe(res => console.log(res))
+
+// Queue async actions
 instance.addToQueue(hello('Hello 1', 3000))
 instance.addToQueue(hello('Hello 2', 1000))
 instance.addToQueue(hello('Hello 3', 2000))
