a57678baa0d99032b2dfd1bceaf738d53a55c8a7
paolomangiadev <paolo.mangia.dev@gmail.com>
2019-10-12 21:40:08 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { mergeMap, delay } from 'rxjs/operators';
+import { Subject, of } from 'rxjs';
+
+class CallsGetQueue {
+  private queue = new Subject();
+  public results;
+  constructor() {
+    this.results = this.queue.pipe(
+      mergeMap((action: any) => action(), null, 1));
+  }
+
+  addToQueue(action) {
+    this.queue.next(action);
+  }
+}
+
+const hello = (str, timeout) => 
+  () => of(str).pipe(delay(timeout));
+const instance = new CallsGetQueue();
+instance.results.subscribe(res => console.log(res))
+instance.addToQueue(hello('Hello 1', 3000))
+instance.addToQueue(hello('Hello 2', 1000))
+instance.addToQueue(hello('Hello 3', 2000))
